82b85896ef305eef68b7bfc3fb7d2c28fe413f52
chiazo <chiazo@live.unc.edu>
2019-05-30 02:18:20 -0700

    attempted to fix highlighting after fixing regex

--- app.ts
+++ app.ts
@@ -6,10 +6,12 @@ let submitButton = document.getElementById("submit_b") as HTMLElement;
 let finishButton = document.getElementById("finish_s") as HTMLElement;
 let restartButton = document.getElementById("restart_s") as HTMLElement;
 let storyInput = document.getElementById("story") as HTMLInputElement;
-
+let spanWords = document.getElementsByTagName("span");
 
 let clickedWord = (event: MouseEvent) => {
-    let clickedWord = (event.target as HTMLDivElement).innerText;
+    let currWord = (event.target as HTMLDivElement)
+    clickedList[clickedList.length] = currWord.innerText;
+    currWord.style.backgroundColor = "yellow";
 };
 
 let saveStory = () => {
@@ -19,60 +21,63 @@ let saveStory = () => {
     let aContainer = document.createElement("div");
     aContainer.className += "  third";
     let aPar = document.createElement("p");
+    aPar.className += " span-par ";
     let specialChar: boolean = false;
     //document.getElementById("storyDisplay")!.appendChild(aPar);
     let storydisplay = document.getElementById("storyDisplay")!;
-    // for (let i = 0; i < words.length;  i++) {
-    //     // console.log(spanWords(words[i], " "));
-    //     let aSpan = document.createElement("span");
-    //     let lastChar = words[i].charAt(words[i].length-1);
-    //     if (/^[,\.!;:]/.test(lastChar)) {
-    //         aSpan.innerText = words[i].substring(0, words[i].length - 1);
-    //         specialChar = true;
-    //     } else {
-    //         aSpan.innerText = words[i] + " "; 
-    //     }
-    //     aSpan.addEventListener("click", function() {
-    //         clickedList[clickedList.length] = aSpan.innerText;
-    //         aSpan.style.backgroundColor = "yellow";
-    //     });
-    //     aPar.appendChild(aSpan);
-
-
-
-    // }
     for (let i = 0; i < words.length; i++) {
         // console.log(spanWords(words[i], " "));
         let specialChar = false;
         let aSpan = document.createElement("span");
-        let format = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
-        if (format.test(words[i].charAt(words[i].length - 1))) {
-            aSpan.innerText = words[i].substring(0, words[i].length-1);
-            specialChar = true;
-        } else {
-            aSpan.innerText = words[i] + " ";
-        }
-        aPar.appendChild(aSpan);
-        if (specialChar) {
-            aPar.innerText += words[i].charAt(words[i].length - 1) + ' ';
-        }
-
-        aSpan.addEventListener("click", function () {
+        
+        aSpan.addEventListener("click", function() {
             clickedList[clickedList.length] = aSpan.innerText;
             aSpan.style.backgroundColor = "yellow";
         });
+        
+        let format = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
+        let s = "";
+        if (format.test(words[i].charAt(words[i].length - 1))) {
+            aSpan.innerText = words[i].substring(0, words[i].length-1);
+            specialChar = true;
+            s = "<span>"+ words[i].substring(0, words[i].length-1) + "</span>" + words[i].charAt(words[i].length - 1);
+        } else {
+            aSpan.innerText = words[i] + " ";
+            s = "<span>"+ words[i] +"</span>"
+        }
+
+       // aPar.appendChild(aSpan);
+        aPar.innerHTML += s + " ";
+        // if (specialChar) {
+        //     aPar.innerText += words[i].charAt(words[i].length - 1) + ' ';
+        // }
 
     }
     aContainer.appendChild(aPar);
     storydisplay.appendChild(aContainer);
-    aContainer.appendChild(aPar);
-    storydisplay.appendChild(aContainer);
+    
+    var parent = document.getElementById("span-par");
+    parent!.addEventListener('click', function(event) {
+        let currWord = (event.target as HTMLDivElement)
+        // if (currWord.tagName === "span") {
+            clickedList[clickedList.length] = currWord.innerText;
+            currWord.style.backgroundColor = "yellow";
+       //  }
+    });
     // document.getElementById("storyDisplay").innerText = input;
 };
 
 let processSubmission = () => {
     console.log("is this working...");
     console.log(storyInput);
+    
+    for (let i = 0; i < spanWords.length; i++) {
+        let w = spanWords[i];
+        w.addEventListener("click", function() {
+            clickedList[clickedList.length] = w.innerText;
+            w.style.backgroundColor = "yellow";
+        });
+    }
 };
 
 restartButton.onclick = (event: MouseEvent) => {
